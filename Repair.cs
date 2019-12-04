54d33101be2b621c4b5a190d2996aae792762a2a
nt153133 <francis.magretto@gmail.com>
2019-12-04 14:00:24 -0500

    Changed to vtable search so should work for CN/NA the same

--- Repair.cs
+++ Repair.cs
@@ -8,13 +8,10 @@ Original work done by Kayla D'orden
 
 using System;
 using System.Linq;
-using System.Reflection;
-using System.Threading;
 using System.Windows.Media;
 using ff14bot;
 using ff14bot.AClasses;
 using ff14bot.Behavior;
-using ff14bot.Enums;
 using ff14bot.Helpers;
 using ff14bot.Managers;
 using ff14bot.RemoteWindows;
@@ -28,9 +25,11 @@ namespace AutoRepair
     {
         public static int RepairThreshold = 30;
         public static int AgentId = 36;
-        private static Language lang;
-        
-        
+
+        private static IntPtr off;
+        private static IntPtr func;
+        private static IntPtr vtable;
+
         private static ActionRunCoroutine s_hook;
 
         private Composite _root;
@@ -39,17 +38,6 @@ namespace AutoRepair
         public override string Name => _Name;
         public override bool WantButton => false;
         public static string _Name => "AutoRepair";
-
-/*        public override void OnPulse()
-        {
-			Log($"Pulse {CreateVendorBehavior.IsRunning}");
-			
-			if (InventoryManager.EquippedItems.Any(item => item.Item != null && item.Item.RepairItemId != 0 && item.Condition < RepairThreshold))
-				Repairing = true;
-            if (!CreateVendorBehavior.IsRunning)
-                CreateVendorBehavior.Start(null);
-        }*/
-
 
         public Composite CreateVendorBehavior
         {
@@ -93,7 +81,7 @@ namespace AutoRepair
                                 new Sequence(
                                     new Action(r => Log("Window open so repairing")),
                                     new Sleep(500),
-                                    new Action(r =>Repair.RepairAll())
+                                    new Action(r => Repair.RepairAll())
                                 )
                             ),
                             new Decorator(r => !Repair.IsOpen,
@@ -110,33 +98,25 @@ namespace AutoRepair
         }
 
         public static bool Repairing { get; set; }
-        // public override void OnButtonPress()
-        // {
-        //    on = !on;
-        //    Logger.Info("Visuals: {0}", on);
-        //    if (on)
-        //        Overlay3D.Drawing += ff14.Visualize;
-        //    else
-        //        Overlay3D.Drawing -= ff14.Visualize;
-        // }
 
         public override void OnInitialize()
         {
             TreeRoot.OnStart += OnBotStart;
             TreeRoot.OnStop += OnBotStop;
             Repairing = false;
-            lang = (Language) typeof(DataManager).GetFields(BindingFlags.Static | BindingFlags.NonPublic).First(i => i.FieldType == typeof(Language)).GetValue(null);
-            if (lang == Language.Chn)
-            {
-                AgentId = 33;
-                Log($"Switching to CN AgentId {AgentId}");
-            }
-            else
-            {
-                Log($"Keeping English AgentId {AgentId}");
-            }
+            var patternFinder = new PatternFinder(Core.Memory);
+            var off = patternFinder.Find("4C 8D 0D ? ? ? ? 45 33 C0 33 D2 48 8B C8 E8 ? ? ? ? Add 3 TraceRelative");
+            var func = patternFinder.Find("48 89 5C 24 ? 57 48 83 EC ? 88 51 ? 49 8B F9");
+            var vtable = patternFinder.Find("48 8D 05 ? ? ? ? 48 89 03 B9 ? ? ? ? 4C 89 43 ? Add 3 TraceRelative");
+
+            var repairAgent = AgentModule.FindAgentIdByVtable(vtable);
+
+            Log($"AgentId {AgentId} Offset {off.ToInt64():X} Func {func.ToInt64():X}");
+
+            AgentId = repairAgent;
+
             _root = new Decorator(r => Repairing, RepairBehavior);
-            //_root = new Decorator(r => Repairing, new ActionAlwaysSucceed());
+
         }
 
         private void OnBotStop(BotBase bot)
@@ -196,10 +176,6 @@ namespace AutoRepair
 
         public static void OpenRepair()
         {
-            var patternFinder = new PatternFinder(Core.Memory);
-            var off = patternFinder.Find("4C 8D 0D ? ? ? ? 45 33 C0 33 D2 48 8B C8 E8 ? ? ? ? Add 3 TraceRelative");
-            var func = patternFinder.Find("48 89 5C 24 ? 57 48 83 EC ? 88 51 ? 49 8B F9");
-
             Core.Memory.CallInjected64<IntPtr>(func, AgentModule.GetAgentInterfaceById(AgentId).Pointer, 0, 0, off);
         }
 
